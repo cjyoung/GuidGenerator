ed63e001da94393865cfe374e5148f98e15cdd32
Chris Young <young.chris.j@gmail.com>
2017-09-23 16:28:06 -0500

    initial check-in

---
+++ GuidGenerator/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+
+namespace GuidGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            btnGenerate_Click(null, null);
+        }
+
+        private void btnGenerate_Click(object sender, RoutedEventArgs e)
+        {
+            txtGuid.Text = Guid.NewGuid().ToString().ToUpper();
+        }
+
+        private void btnCopy_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(txtGuid.Text);
+        }
+    }
+}
